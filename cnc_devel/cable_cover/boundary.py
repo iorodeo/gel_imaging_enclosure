2fd1341c44654bb36c817be64ce4aeaa48c76a6d
Will Dickson <will@iorodeo.com>
2015-07-07 22:52:55 -0700

    Added gcode for cutting modifications for initial prototype.

---
+++ cnc_devel/cable_cover/boundary.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+import os 
+import sys
+from py2gcode import gcode_cmd
+from py2gcode import cnc_dxf
+
+feedrate = 150.0
+fileName = 'cable_cover.dxf'
+depth = 0.27
+startZ = 0.0
+safeZ = 0.5
+maxCutDepth = 0.08
+toolDiam =  0.0425 
+direction = 'ccw'
+cutterComp = 'outside'
+#cutterComp = None 
+startDwell = 1.0
+startCond = 'minX'
+
+layerList = ['boundary']
+
+prog = gcode_cmd.GCodeProg()
+prog.add(gcode_cmd.GenericStart())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.FeedRate(feedrate))
+prog.add(gcode_cmd.PathBlendMode(p=0.02,q=0.01))
+
+param = {
+        'fileName'    : fileName,
+        'dxfTypes'    : ['LINE', 'ARC', 'CIRCLE'],
+        'layers'      : layerList,
+        'depth'       : depth,
+        'startZ'      : startZ,
+        'safeZ'       : safeZ,
+        'toolDiam'    : toolDiam,
+        'direction'   : direction,
+        'cutterComp'  : cutterComp,
+        'maxCutDepth' : maxCutDepth,
+        'startDwell'  : startDwell, 
+        'startCond'   : startCond,
+        }
+boundary = cnc_dxf.DxfBoundary(param)
+prog.add(boundary)
+
+prog.add(gcode_cmd.ExactPathMode())
+
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.End(),comment=True)
+baseName, dummy = os.path.splitext(__file__)
+fileName = '{0}.ngc'.format(baseName)
+print('generating: {0}'.format(fileName))
+prog.write(fileName)
