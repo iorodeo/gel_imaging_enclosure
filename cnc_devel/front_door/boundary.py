be64dfc3c9186348e6ed734bb5fd8f94631b14e6
Will Dickson <will@iorodeo.com>
2015-12-14 07:54:23 -0800

    Finished gcode generation for modified right side wall.

--- cnc_devel/front_door/boundary.py
+++ cnc_devel/front_door/boundary.py
@@ -22,40 +22,22 @@ prog.add(gcode_cmd.Space())
 prog.add(gcode_cmd.FeedRate(feedrate))
 prog.add(gcode_cmd.PathBlendMode(p=0.02,q=0.01))
 
-if True:
-    param = {
-            'fileName'    : fileName,
-            'dxfTypes'    : ['LINE', 'ARC', 'CIRCLE'],
-            'layers'      : ['camera_hole'],
-            'depth'       : depth,
-            'startZ'      : startZ,
-            'safeZ'       : safeZ,
-            'toolDiam'    : toolDiam,
-            'direction'   : direction,
-            'cutterComp'  : 'inside',
-            'maxCutDepth' : maxCutDepth,
-            'startDwell'  : startDwell, 
-            }
-    circle = cnc_dxf.DxfCircBoundary(param)
-    prog.add(circle)
-
-if True:
-    param = {
-            'fileName'    : fileName,
-            'dxfTypes'    : ['LINE', 'ARC', 'CIRCLE'],
-            'layers'      : ['boundary'],
-            'depth'       : depth,
-            'startZ'      : startZ,
-            'safeZ'       : safeZ,
-            'toolDiam'    : toolDiam,
-            'direction'   : direction,
-            'cutterComp'  : 'outside',
-            'maxCutDepth' : maxCutDepth,
-            'startDwell'  : startDwell, 
-            'startCond'   : startCond,
-            }
-    boundary = cnc_dxf.DxfBoundary(param)
-    prog.add(boundary)
+param = {
+        'fileName'    : fileName,
+        'dxfTypes'    : ['LINE', 'ARC', 'CIRCLE'],
+        'layers'      : ['boundary'],
+        'depth'       : depth,
+        'startZ'      : startZ,
+        'safeZ'       : safeZ,
+        'toolDiam'    : toolDiam,
+        'direction'   : direction,
+        'cutterComp'  : 'outside',
+        'maxCutDepth' : maxCutDepth,
+        'startDwell'  : startDwell, 
+        'startCond'   : startCond,
+        }
+boundary = cnc_dxf.DxfBoundary(param)
+prog.add(boundary)
 
 prog.add(gcode_cmd.ExactPathMode())
 
