a2fbcbcaaccdb8b88a8b8108052abfe6ff12cdf2
Will Dickson <will@iorodeo.com>
2016-03-16 19:28:26 -0700

    Added gcode for cutting dslr version of gel imaging enclosure.

---
+++ cnc/dslr_camera/camera_mount/boundary.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+import os 
+import sys
+from py2gcode import gcode_cmd
+from py2gcode import cnc_dxf
+
+feedrate = 150.0
+fileName = 'camera_mount_plate.dxf'
+depth = 0.30
+startZ = 0.0
+safeZ = 0.5
+maxCutDepth = 0.08
+toolDiam = 0.25 
+direction = 'ccw'
+startDwell = 1.0
+startCond = 'minX'
+
+
+prog = gcode_cmd.GCodeProg()
+prog.add(gcode_cmd.GenericStart())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.FeedRate(feedrate))
+prog.add(gcode_cmd.PathBlendMode(p=0.02,q=0.01))
+
+
+param = {
+        'fileName'    : fileName,
+        'dxfTypes'    : ['LINE', 'ARC', 'CIRCLE'],
+        'layers'      : ['boundary'],
+        'depth'       : depth,
+        'startZ'      : startZ,
+        'safeZ'       : safeZ,
+        'toolDiam'    : toolDiam,
+        'direction'   : direction,
+        'cutterComp'  : 'outside',
+        'maxCutDepth' : maxCutDepth,
+        'startDwell'  : startDwell, 
+        'startCond'   : startCond,
+        }
+boundary = cnc_dxf.DxfBoundary(param)
+prog.add(boundary)
+
+prog.add(gcode_cmd.ExactPathMode())
+
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.End(),comment=True)
+baseName, dummy = os.path.splitext(__file__)
+fileName = '{0}.ngc'.format(baseName)
+print('generating: {0}'.format(fileName))
+prog.write(fileName)
